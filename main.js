887e0cfe6adea5c5153d0bca0fbc0c9c47802427
Luis Amaison <luis.unlam@gmail.com>
2022-03-29 23:36:47 -0300

    Clase 4 - Simulador Interactivo

---
+++ main.js
@@ -0,0 +1,18 @@
+/* Comienzo del programa */
+
+/* Calculadora de intereses de plazo fijo */
+
+
+let dbNombre = "Luis";
+let dbNumeroCuenta = 123456;
+
+
+do {
+    let cliente = prompt("Bienvenido al Banco Santander Rio.\nPor favor, ingrese su Nombre");
+    let numeroCuenta = prompt("Ingrese su numero de cuenta");
+
+    if(cliente === dbNombre && numeroCuenta === dbNumeroCuenta)
+
+} while ()
+
+
